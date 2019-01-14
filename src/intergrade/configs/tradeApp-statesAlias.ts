03d3328873053d507f70e4846005cabecdbf5215
niaoge <78493244@qq.com>
2019-01-15 02:58:14 +0800

    readme

--- src/intergrade/configs/tradeApp-statesAlias.ts
+++ src/intergrade/configs/tradeApp-statesAlias.ts
@@ -5,23 +5,23 @@
  *  由 [stategen.progen]代码生成器创建，不要手动修改,否则将在下次创建时自动覆盖
  */
 import {MsgState} from "../interfaces/MsgFaces";
+import {User_meState} from "../interfaces/User_meFaces";
+import {Topic_detail_$topicIdState} from "../interfaces/Topic_detail_$topicIdFaces";
+import {AppState} from "../interfaces/AppFaces";
+import {HomeState} from "../interfaces/HomeFaces";
 import {User_detail_$usernameState} from "../interfaces/User_detail_$usernameFaces";
 import {TopicState} from "../interfaces/TopicFaces";
-import {AppState} from "../interfaces/AppFaces";
 import {Topic_publishState} from "../interfaces/Topic_publishFaces";
-import {User_meState} from "../interfaces/User_meFaces";
-import {HomeState} from "../interfaces/HomeFaces";
-import {Topic_detail_$topicIdState} from "../interfaces/Topic_detail_$topicIdFaces";
 import {LoginState} from "../interfaces/LoginFaces";
 
 export default interface StatesAlias {
   msg: MsgState;
+  user_me: User_meState;
+  topic_detail_$topicId: Topic_detail_$topicIdState;
+  app: AppState;
+  home: HomeState;
   user_detail_$username: User_detail_$usernameState;
   topic: TopicState;
-  app: AppState;
   topic_publish: Topic_publishState;
-  user_me: User_meState;
-  home: HomeState;
-  topic_detail_$topicId: Topic_detail_$topicIdState;
   login: LoginState;
 }
