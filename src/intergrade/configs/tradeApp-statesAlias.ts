e73b107cb6583fa72c66217862665c88763fd4fb
niaoge <78493244@qq.com>
2019-01-18 05:55:17 +0800

    2.0.2.stategen.RELEASE

--- src/intergrade/configs/tradeApp-statesAlias.ts
+++ src/intergrade/configs/tradeApp-statesAlias.ts
@@ -4,24 +4,24 @@
  *  created by [stategen.progen] ,do not edit it manually otherwise your code will be override by next call progen,
  *  由 [stategen.progen]代码生成器创建，不要手动修改,否则将在下次创建时自动覆盖
  */
-import {MsgState} from "../interfaces/MsgFaces";
-import {User_meState} from "../interfaces/User_meFaces";
 import {Topic_detail_$topicIdState} from "../interfaces/Topic_detail_$topicIdFaces";
-import {AppState} from "../interfaces/AppFaces";
-import {HomeState} from "../interfaces/HomeFaces";
-import {User_detail_$usernameState} from "../interfaces/User_detail_$usernameFaces";
+import {User_meState} from "../interfaces/User_meFaces";
 import {TopicState} from "../interfaces/TopicFaces";
+import {HomeState} from "../interfaces/HomeFaces";
 import {Topic_publishState} from "../interfaces/Topic_publishFaces";
+import {User_detail_$usernameState} from "../interfaces/User_detail_$usernameFaces";
+import {MsgState} from "../interfaces/MsgFaces";
+import {AppState} from "../interfaces/AppFaces";
 import {LoginState} from "../interfaces/LoginFaces";
 
 export default interface StatesAlias {
-  msg: MsgState;
-  user_me: User_meState;
   topic_detail_$topicId: Topic_detail_$topicIdState;
-  app: AppState;
-  home: HomeState;
-  user_detail_$username: User_detail_$usernameState;
+  user_me: User_meState;
   topic: TopicState;
+  home: HomeState;
   topic_publish: Topic_publishState;
+  user_detail_$username: User_detail_$usernameState;
+  msg: MsgState;
+  app: AppState;
   login: LoginState;
 }
--- src/pages/topic/model.ts
+++ src/pages/topic/model.ts
@@ -1,9 +1,19 @@
-import {topicModel} from "@i/models/TopicModel";
+import {TopicCommand, topicModel} from "@i/models/TopicModel";
 import {TopicDispatch, TopicModel, TopicState} from "@i/interfaces/TopicFaces";
 import {topicCustomState} from "@pages/topic/TopicCustomFaces";
 import TopicType from "@i/enums/TopicType";
 import {PaginationProps} from "antd/lib/pagination";
+import {Action, SagaCommands} from "@utils/DvaUtil";
 
+
+
+topicModel.effects.getTopicPageList =function* ({type, payload, error}, {put, call, select}): any{
+  let topicPageListEffect = TopicCommand.getTopicPageList_effect({type, payload, error}, {put, call, select});
+
+
+
+
+}
 
 
 export default topicModel;
